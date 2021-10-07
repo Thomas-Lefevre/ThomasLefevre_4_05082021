99fc75add85043dc4b0c787f043e78dcf87ddd39
thomas lefevre <thomas.lefevre4652@gmail.com>
2021-10-07 16:41:14 +0200

    correction titre css et fermeture modal

--- modal.js
+++ modal.js
@@ -10,6 +10,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalSubmit = document.querySelector(".btn-submit");
+const modalForm = document.getElementById("modalForm");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
 const iconCloseThank = document.querySelector(".iconCloseThank");
@@ -45,7 +46,7 @@ thankModalBtn.addEventListener('click', closeThankModal);
 iconCloseThank.addEventListener('click', closeThankModal);
 
 //soumettre le formulaire
-modalSubmit.addEventListener("click",validation);
+modalSubmit.addEventListener("click", validation);
 
 // launch modal form
 function launchModal() {
@@ -58,87 +59,88 @@ function closeModal() {
 }
 
 // fermer la modal de confirmation 
-function closeThankModal(){
-  thankModal.style.display= "none";
+function closeThankModal() {
+  thankModal.style.display = "none";
 }
 
 //validation
-function validation(event){
-  let erreur = 0 ;
+function validation(event) {
+  let erreur = 0;
   //si le champ prenom est vide
-  if (formPrenom.validity.valueMissing){
+  if (formPrenom.validity.valueMissing) {
     event.preventDefault();
-    prenomInvalide.textContent= "Veuillez entrer un prénom.";
+    prenomInvalide.textContent = "Veuillez entrer un prénom.";
     formPrenom.style.border = "2px solid red"
-    prenomInvalide.style.fontSize= "12px"
+    prenomInvalide.style.fontSize = "12px"
     prenomInvalide.style.color = "red";
-    erreur ++;
+    erreur++;
   };
   //si le champ prenom est trop court
-  if (formPrenom.validity.tooShort){
+  if (formPrenom.validity.tooShort) {
     event.preventDefault();
-    prenomInvalide.textContent= "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    prenomInvalide.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     formPrenom.style.border = "2px solid red"
-    prenomInvalide.style.fontSize= "12px"
+    prenomInvalide.style.fontSize = "12px"
     prenomInvalide.style.color = "red";
-    erreur ++;
+    erreur++;
   };
   //si le champ nom est vide
-  if(formNom.validity.valueMissing){
+  if (formNom.validity.valueMissing) {
     event.preventDefault();
-    nomInvalide.textContent="Veuillez entrer un nom de famille.";
+    nomInvalide.textContent = "Veuillez entrer un nom de famille.";
     formNom.style.border = "2px solid red"
-    nomInvalide.style.fontSize= "12px"
+    nomInvalide.style.fontSize = "12px"
     nomInvalide.style.color = "red";
-    erreur ++;
+    erreur++;
   };
   //si le champ nom est trop court
-  if (formNom.validity.tooShort){
+  if (formNom.validity.tooShort) {
     event.preventDefault();
-    nomInvalide.textContent= "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    nomInvalide.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     formNom.style.border = "2px solid red"
-    nomInvalide.style.fontSize= "12px"
+    nomInvalide.style.fontSize = "12px"
     nomInvalide.style.color = "red";
-    erreur ++;
+    erreur++;
   };
   //si le champ email n'est pas valide
-  if((formEmail.validity.valid)==false){
+  if ((formEmail.validity.valid) == false) {
     event.preventDefault();
-    emailInvalide.textContent="Veuillez entrer une adresse email valide";
+    emailInvalide.textContent = "Veuillez entrer une adresse email valide";
     formEmail.style.border = "2px solid red"
-    emailInvalide.style.fontSize= "12px"
-    emailInvalide.style.color="red";
-    erreur ++;
+    emailInvalide.style.fontSize = "12px"
+    emailInvalide.style.color = "red";
+    erreur++;
   };
   //si la date est vide
-  if (formDate.validity.valueMissing){
+  if (formDate.validity.valueMissing) {
     event.preventDefault();
-    dateInvalide.textContent= "Veuillez entrer une date de naissance.";
+    dateInvalide.textContent = "Veuillez entrer une date de naissance.";
     formDate.style.border = "2px solid red"
-    dateInvalide.style.fontSize= "12px"
+    dateInvalide.style.fontSize = "12px"
     dateInvalide.style.color = "red";
-    erreur ++;
+    erreur++;
   };
   //si ce n'est pas un nombre
-  if((formQuantity.validity.valid)==false){
+  if ((formQuantity.validity.valid) == false) {
     event.preventDefault();
-    quantityInvalide.textContent="Veuillez entrer un nombre";
+    quantityInvalide.textContent = "Veuillez entrer un nombre";
     formQuantity.style.border = "2px solid red"
-    quantityInvalide.style.fontSize= "12px"
-    quantityInvalide.style.color="red";
-    erreur ++;
+    quantityInvalide.style.fontSize = "12px"
+    quantityInvalide.style.color = "red";
+    erreur++;
   };
   //si la case conditions générales n'est pas cochée
-  if(formCondition.checked == false){
+  if (formCondition.checked == false) {
     event.preventDefault();
-    conditionInvalide.textContent="Veuillez accepter les conditions d'utilisation";
+    conditionInvalide.textContent = "Veuillez accepter les conditions d'utilisation";
     formCondition.style.border = "2px solid red"
-    conditionInvalide.style.fontSize= "12px"
-    conditionInvalide.style.color="red";
-    erreur ++;
+    conditionInvalide.style.fontSize = "12px"
+    conditionInvalide.style.color = "red";
+    erreur++;
   };
-  if (erreur ===0){
-      thankModal.style.display = "block";
-      event.preventDefault();
+  if (erreur === 0) {
+    modalForm.style.display = "none";
+    thankModal.style.display = "block";
+    event.preventDefault();
   }
 }
